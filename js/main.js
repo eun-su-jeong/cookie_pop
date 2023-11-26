2a9ba8d2228d6b213499f0dfe57dc793cbbbd8b3
eun-su-jeong <dmswid0@naver.com>
2023-11-26 11:38:29 +0900

    쿠키 기능유무 선택가능하게 코드 개선

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-const [view, del] = document.querySelectorAll('nav button');
 const lastTime = 2;
+const wid = 600;
+const bg = 'aqua';
+let enableCookie = true;
 
-if (document.cookie.indexOf('today=done') < 0) {
-	createPop({ wid: 600, bg: 'pink', lastTime: lastTime });
+//setCookie('today', 'done', 0);
+initCookie();
+
+function initCookie() {
+	if (document.cookie.indexOf('today=done') < 0) {
+		createPop({ wid: wid, bg: bg, lastTime: lastTime });
+	}
 }
-
-//쿠키 확인
-view.addEventListener('click', () => console.log(document.cookie));
-
-del.addEventListener('click', () => {
-	setCookie('today', 'done', 0);
-	alert('쿠키 삭제');
-});
 
 //쿠키생성 함수
 function setCookie(name, value, expires) {
 	let now = new Date();
-	//현재 밀리세컨드 시간값에 1시간(1000*60*60) * expires
 	let duedate = now.getTime() + 1000 * 60 * 60 * expires;
 	now.setTime(duedate);
 	document.cookie = `${name}=${value}; path=/; expires=${now.toUTCString()}`;
@@ -28,13 +26,17 @@ function createPop({ wid = 400, bg = '#ddd', lastTime = 24 }) {
 	aside.style.width = wid + 'px';
 	aside.style.backgroundColor = bg;
 
+	const checkTags = `
+    <p>
+      <input type="checkbox" id="ck">
+      <label for="ck">${lastTime}시간동안 보지 않기</label>
+    </p>
+  `;
+
 	const tags = `
     <div class="con"></div>
     <div class="controls">
-      <p>
-        <input type="checkbox" id="ck">
-        <label for="ck">${lastTime}시간동안 보지 않기</label>
-      </p>
+      ${enableCookie ? checkTags : ''}      
       <button>CLOSE</button>
     </div>
   `;
@@ -42,11 +44,13 @@ function createPop({ wid = 400, bg = '#ddd', lastTime = 24 }) {
 	document.body.append(aside);
 
 	const close = document.querySelector('aside button');
-	const ck = document.querySelector('aside #ck');
 
 	//팝업 닫기
 	close.addEventListener('click', () => {
-		if (ck.checked) setCookie('today', 'done', lastTime);
+		if (enableCookie) {
+			const ck = document.querySelector('aside #ck');
+			if (ck.checked) setCookie('today', 'done', lastTime);
+		}
 		document.querySelector('aside').remove();
 	});
 }
