38b812d455453558da2fb60b8721f7426f1c4d99
eun-su-jeong <dmswid0@naver.com>
2023-11-26 09:50:36 +0900

    팝업 닫기 시 체크박스 선택시 쿠키 생성

--- js/main.js
+++ js/main.js
@@ -3,14 +3,21 @@ const popup = document.querySelector('aside');
 const ck = popup.querySelector('#ck');
 const close = popup.querySelector('button');
 
+// 쿠키 확인
+view.addEventListener('click', () => console.log(document.cookie));
+
+// 팝업 열기
 open.addEventListener('click', () => {
 	popup.style.display = 'block';
 });
 
+// 팝업 닫기
 close.addEventListener('click', () => {
+	if (ck.checked) setCookie('today', 'done', 1);
 	popup.style.display = 'none';
 });
 
+//쿠키생성 함수
 function setCookie(name, value, expires) {
 	let today = new Date();
 	let duedate = today.getDate() + expires;
