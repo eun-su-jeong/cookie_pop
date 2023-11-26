74f4abfeddfaf510c11f640d1c8712d1a4e7fc2e
eun-su-jeong <dmswid0@naver.com>
2023-11-26 11:21:37 +0900

    쿠키팝업 기능 개선

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-const [open, view, del] = document.querySelectorAll('nav button');
-const popup = document.querySelector('aside');
-const ck = popup.querySelector('#ck');
-const close = popup.querySelector('button');
+const [view, del] = document.querySelectorAll('nav button');
+const lastTime = 2;
 
-document.cookie.indexOf('today=done') < 0 ? (popup.style.display = 'block') : (popup.style.display = 'none');
-// -1 을 반환 : 쿠키가 없는걸 판단
+if (document.cookie.indexOf('today=done') < 0) {
+	createPop({ wid: 600, bg: 'pink', lastTime: lastTime });
+}
 
-// 쿠키 확인
+//쿠키 확인
 view.addEventListener('click', () => console.log(document.cookie));
 
 del.addEventListener('click', () => {
@@ -14,22 +13,40 @@ del.addEventListener('click', () => {
 	alert('쿠키 삭제');
 });
 
-// 팝업 열기
-open.addEventListener('click', () => {
-	popup.style.display = 'block';
-});
-
-// 팝업 닫기
-close.addEventListener('click', () => {
-	if (ck.checked) setCookie('today', 'done', 10);
-	popup.style.display = 'none';
-});
-
 //쿠키생성 함수
 function setCookie(name, value, expires) {
 	let now = new Date();
-	// 현재 밀리세컨드 시간값에 1시간(1000 * 60 * 60) * expires
+	//현재 밀리세컨드 시간값에 1시간(1000*60*60) * expires
 	let duedate = now.getTime() + 1000 * 60 * 60 * expires;
 	now.setTime(duedate);
 	document.cookie = `${name}=${value}; path=/; expires=${now.toUTCString()}`;
+}
+
+//팝업생성 함수
+function createPop({ wid = 400, bg = '#ddd', lastTime = 24 }) {
+	const aside = document.createElement('aside');
+	aside.style.width = wid + 'px';
+	aside.style.backgroundColor = bg;
+
+	const tags = `
+    <div class="con"></div>
+    <div class="controls">
+      <p>
+        <input type="checkbox" id="ck">
+        <label for="ck">${lastTime}시간동안 보지 않기</label>
+      </p>
+      <button>CLOSE</button>
+    </div>
+  `;
+	aside.innerHTML = tags;
+	document.body.append(aside);
+
+	const close = document.querySelector('aside button');
+	const ck = document.querySelector('aside #ck');
+
+	//팝업 닫기
+	close.addEventListener('click', () => {
+		if (ck.checked) setCookie('today', 'done', lastTime);
+		document.querySelector('aside').remove();
+	});
 }
