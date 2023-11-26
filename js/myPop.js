b2c4e41264cee8e7ca6ca7345a2da07e97a40167
eun-su-jeong <dmswid0@naver.com>
2023-11-26 12:02:40 +0900

    쿠키 옵션 설정

--- js/myPop.js
+++ js/myPop.js
@@ -1,11 +1,13 @@
 class MyPop {
 	constructor(opt) {
-		this.lastTime = 2;
-		this.wid = opt.wid;
-		this.bg = opt.bg;
-		this.enableCookie = opt.enableCookie;
+		const defOpt = { wid: 400, bg: 'gray', enableCookie: true, lastTime: 24 };
+		const resultOpt = { ...defOpt, ...opt };
+		this.lastTime = resultOpt.lastTime;
+		this.wid = resultOpt.wid;
+		this.bg = resultOpt.bg;
+		this.enableCookie = resultOpt.enableCookie;
 
-		// this.setCookie('today', 'done', 0);
+		//setCookie('today', 'done', 0);
 		this.initCookie();
 	}
 	initCookie() {
@@ -13,7 +15,15 @@ class MyPop {
 			this.createPop({ wid: this.wid, bg: this.bg, lastTime: this.lastTime });
 		}
 	}
-	createPop({ wid = 400, bg = '#ddd', lastTime = 24 }) {
+
+	setCookie(name, value, expires) {
+		let now = new Date();
+		let duedate = now.getTime() + 1000 * 60 * 60 * expires;
+		now.setTime(duedate);
+		document.cookie = `${name}=${value}; path=/; expires=${now.toUTCString()}`;
+	}
+
+	createPop() {
 		const aside = document.createElement('aside');
 		aside.style.width = this.wid + 'px';
 		aside.style.backgroundColor = this.bg;
@@ -21,14 +31,14 @@ class MyPop {
 		const checkTags = `
       <p>
         <input type="checkbox" id="ck">
-        <label for="ck">${lastTime}시간동안 보지 않기</label>
+        <label for="ck">${this.lastTime}시간동안 보지 않기</label>
       </p>
     `;
 
 		const tags = `
       <div class="con"></div>
       <div class="controls">
-        ${enableCookie ? checkTags : ''}      
+        ${this.enableCookie ? checkTags : ''}      
         <button>CLOSE</button>
       </div>
     `;
@@ -39,17 +49,11 @@ class MyPop {
 
 		//팝업 닫기
 		close.addEventListener('click', () => {
-			if (enableCookie) {
+			if (this.enableCookie) {
 				const ck = document.querySelector('aside #ck');
-				if (ck.checked) setCookie('today', 'done', lastTime);
+				if (ck.checked) setCookie('today', 'done', this.lastTime);
 			}
 			document.querySelector('aside').remove();
 		});
-	}
-	setCookie(name, value, expires) {
-		let now = new Date();
-		let duedate = now.getTime() + 1000 * 60 * 60 * expires;
-		now.setTime(duedate);
-		document.cookie = `${name}=${value}; path=/; expires=${now.toUTCString()}`;
 	}
 }
